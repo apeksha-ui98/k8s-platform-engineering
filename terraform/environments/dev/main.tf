a1d838a9ea734701f12a931c1449d385840d58a2
apeksha-ui98 <alalslbl82@gmail.com>
2026-06-07 16:05:39 +0530

    k8s-platform-engineering project

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -6,3 +6,16 @@ module "ecr" {
 output "repository_url" {
   value = module.ecr.repository_url
 }
+module "vpc" {
+  source = "../../modules/vpc"
+
+  environment = "dev"
+}
+
+module "eks" {
+  source = "../../modules/eks"
+
+  cluster_name = "platform-eks"
+
+  subnet_ids = module.vpc.public_subnet_ids
+}
